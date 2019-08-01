16eac0c935e3ae05ea78b59d7cfca90e0b097286
bibhu <prasadpal107.com>
2019-08-01 09:10:26 +0530

    Login-form and all dependencies

---
+++ frontend/allDependencies.sh
@@ -0,0 +1,11 @@
+#install_NodeJs and then React
+sudo npm install npm@latest -g  
+sudo npm install -g create-react-app
+sudo npm i --save @fortawesome/react-fontawesome
+sudo npm install @fortawesome/fontawesome-free
+sudo npm i --save @fortawesome/free-solid-svg-icons
+sudo npm install bootstrap       
+sudo npm i --save @fortawesome/fontawesome-svg-core
+sudo npm install bootstrap-css-only
+sudo npm install --save mdbreact
+sudo npm install --save reactstrap react react-dom
